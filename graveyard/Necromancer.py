21b2f7233fac907cc71a6710d0c62a0249427827
沈宁 <ning.shen@tongdun.net>
2024-04-23 17:45:59 +0800

    v0.01

--- graveyard/Necromancer.py
+++ graveyard/Necromancer.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import json
+import json , random
 from datetime import datetime
 
 
@@ -22,37 +22,80 @@ def Raise_dead(data_conf , start_datetime , freq , rows):
 def __dispatcher(item , rows):
     if item['func'] == 'F_RAND_STR':
         return __rand_str(rows)
+    #
     elif item['func'] == 'F_RAND_NUM':
         return __rand_num(rows)
+    #
     elif item['func'] == 'F_CONST':
-        if item['input'] is not None :
+        if item.__contains__('input') and  item['input'] is not None :
             return __const(item['input'] , rows)
         else:
             return __const(item['name'] , rows)
+    #
+    elif item['func'] == 'F_COUNT':
+        default_count = 3
+        if item.__contains__('input') and  item['input'] is not None :
+            default_count = item['input']
+        if item.__contains__('prefix') and  item['prefix'] is not None :
+            return __count(item['prefix'] , int(default_count) , rows)
+        else:
+            return __count( None , int(default_count) , rows)
+    #
+    elif item['func'] == 'F_SEQ':
+        default_begin = 1
+        if item.__contains__('input') and  item['input'] is not None :
+            default_begin = item['input']
+        if item.__contains__('prefix') and  item['prefix'] is not None :
+            return __seq(item['prefix'] , int(default_begin) , rows)
+        else:
+            return __seq( None , int(default_begin) , rows)
+    
     elif item['func'] == 'F_EMPTY':
         return ""
+    
     elif item['func'] == 'F_STAT_SUM':
-        return __sum(50000 , rows)
+        if item.__contains__('input') and  item['input'] is not None :
+            return __sum(int(item['input']) , rows)
+        else:
+            return __sum(50000 , rows)
     elif item['func'] == 'F_STAT_AVG':
-        return __avg(10000 , rows)
+        return __sum(10000 , rows)
+    
+    elif item['func'] == 'F_STAT_TREND':
+        init_val = 10000
+        delta = "1%"
+        if item.__contains__('input') and  item['input'] is not None :
+            init_val = float(item['input'])
+        if item.__contains__('prefix') and  item['prefix'] is not None :
+            delta =  item['prefix'] 
+        return __trend(init_val , delta , rows)
+    elif item['func'] == 'F_RAND_IP':
+        return __rand_ip(rows)
+    elif item['func'] == 'F_RAND_PHONE':
+        return __rand_phone(rows)
     else:
         return ""
     
 
 def __datetime(start_date , freq_arg , rows):
-    freq = pd.offsets.Day()
-    if freq_arg == "M" :
-        freq = pd.offsets.Minute()
-    elif freq_arg == "H":
-        freq = pd.offsets.Hour()
-    return pd.date_range(start_date ,periods=rows , freq=freq)
+    return pd.date_range(start_date , periods=rows , freq=freq_arg )
 
 def __const(const , rows):
     return pd.Series([const]*rows)
 
-def __count(cnt , rows):
-    data = np.random.randint(1 , cnt+2, size=rows)
-    return pd.Series(data)
+
+def __seq(prefix  , begin , rows):
+    arr = np.arange(int(begin), int(begin) + rows)
+    if prefix is not None:
+        arr= np.array([prefix + '{}'.format(i) for i in arr])
+    s = pd.Series(arr)
+    return s
+
+def __count(prefix , cnt , rows ):
+    arr = np.random.randint(1 , cnt+1, size=rows)
+    if prefix is not None:
+         arr= np.array([prefix + '{}'.format(i) for i in arr])
+    return pd.Series(arr)
 
 def __rand_str(rows):
     return pd.Series(np.random.choice(list('abcdefghijklmnopqrstuvwxyz'), size=rows))
@@ -73,20 +116,48 @@ def __sum(sum , rows):
 def __avg(avg , rows):
     pass
 
+def __trend(init_val , delta , rows):
+    # delta = str()
+    if delta.find('%') != -1:
+        pct = float(delta[:-1])/100.0 + 1.0
+        arr = [pct ** (i+1) for i in range(rows)]
+        # print(arr)
+        s = pd.Series(arr) * init_val
+        return s.round(2)
+    else:
+        arr = [ float(delta)*(i+1)  for i in range(rows)]
+        # print(arr)
+        # return pd.Series(arr).cumsum() + init_val
+        return pd.Series(arr) + init_val
+    
+
 def __inc(init_val , rows):
     decrease_factors = [0.95 ** i for i in range(rows)]
     arr = pd.Series(decrease_factors).cumsum() + init_val
     return arr
 
-def __seq(prefix  , rows , begin):
-    arr = np.arange(begin, begin + rows)
-    s = pd.Series(arr)
-    s = s.apply(lambda x: prefix + str(x))
-    return s
+def __rand_ip(rows):
+    arr = [".".join(str(random.randint(0, 255)) for _ in range(4)) for _ in range(rows)]
+    return pd.Series(arr)
+
+def __rand_phone(rows):
+    def generate_random_phone_number():
+        prefix = ['130', '131', '132', '145', '150', '151', '152', '153', '155', '156', '157', '158', '159', '170', '176', '177', '178', '180', '181', '182', '183', '184', '185', '186', '187', '188', '189']
+        p = random.choice(prefix)
+        num = random.randint(10000000, 99999999)
+        return p + str(num)
+    return pd.Series([generate_random_phone_number() for _ in range(rows)])
+
+def __distrib(vals_ary , ratios , rows):
+    cum_ratios = np.array(ratios) / sum(ratios)
+    # print(cum_ratios)
+    arr = np.random.choice(vals_ary, size=rows, p=cum_ratios)
+    return pd.Series(arr)
 
 
 if __name__ == "__main__":
-    arr = __const("abc" , 5)
-    t = {}
-    t["a"] = arr
-    print(pd.DataFrame({"index" : list("abcde") , "value":arr} ))
+    # print(__seq("seq_0000" , 1000 , 10))
+
+    a = '110%'
+    print(__trend(10000 , "-10%" , 10))
+    # print(__distrib(["a","bb","ccc"] , [2 , 1 , 1], 10) )
--- static/libs/zig.js
+++ static/libs/zig.js
@@ -4,22 +4,31 @@ new Vue({
     delimiters:['_%','%_'],
     data() {
         return {
-          continue_time : false,
+          centerDialogVisible : false, // see all dialog flag
+          continue_time : false, // time set switch
           data_headers:[],
-          tableData: [{
-            
-          }],
-          retData :[],
-          isdrawershow:false,
-          freq: 'D',
+          tableData: [{}], // column configs
+          retData :[],// temp result [[0][...] , [1][...],...]
+          totalData:[],//for see all
+          isdrawershow:false, // temp result drawer flag
+          freq: 'MIN', //time unit
+          step: 1, //time step
           value: '',
-          date_start: '2024-01-01 10:10:10',
+          date_start: '2024-01-01 10:10:10', // time val
           num: "10",
-          data_cols:"",
-          data_rows:""
+          data_cols:"", // header input
+          data_cols_disabled:false, // header input editable
+          data_rows: 1 // summon rows
         }
       },
+      computed:{
+        arraySum() {
+          return this.retData.reduce((sum, item) => sum + item.length, 0);
+        },
+      },
+
       methods: {
+        //handle column input change event
         handleInputCol(val){
             this.tableData.length=0;
             fields = val.split(",")
@@ -29,38 +38,142 @@ new Vue({
             // console.log(this.data_headers)
         },
 
-
+        // reset btn click
         reset(){
             this.retData = [];
             this.tableData = [{}];
             this.data_cols = "";
+            this.data_cols_disabled = false;
+        },
+        // dispel btn click
+        dropData(){
+          this.retData = [];
         },
 
+        // check btn click
         showRet(){
           this.isdrawershow = true;
         },
 
+        //time switch on change 
+        changeST(){
+          if(this.continue_time){
+            this.extend_time();
+          }
+        },
+
+        // format js Date to Str "yyyy-MM-dd HH:mm:ss"
+        formatDate(date) {
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, '0');
+          const day = String(date.getDate()).padStart(2, '0');
+          const hours = String(date.getHours()).padStart(2, '0');
+          const minutes = String(date.getMinutes()).padStart(2, '0');
+          const seconds = String(date.getSeconds()).padStart(2, '0');
+          return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        },
+
+        //set st for lastes records timestamp
+        extend_time(){
+           var target = "2000-01-01 00:00:00";
+           for(var i=0;i<this.retData.length;i++){
+              var batch = this.retData[i];
+              for(var j = 0 ; j<batch.length;j++){
+                var item = batch[j];
+                if(item['index'] > target){
+                  target = item['index']
+                }
+              }
+           }
+          
+           
+           var currentDate = new Date(target);
+          //  console.log("=", currentDate , this.freq)
+           if(this.freq =="H"){
+            currentDate = new Date(currentDate.setHours(currentDate.getHours() + 1));
+           }else if(this.freq =="MIN"){
+            currentDate = new Date(currentDate.setMinutes(currentDate.getMinutes() + 1));
+           }else if(this.freq =="S"){
+            currentDate = new Date(currentDate.setSeconds(currentDate.getSeconds() + 1));
+           }else if(this.freq =="D"){
+            currentDate = new Date(currentDate.setDate(currentDate.getDay() + 1));
+           }
+          //  console.log("==", currentDate , this.freq)
+           this.date_start = this.formatDate(currentDate);
+
+        },
+
+        
+        //summon
         apply(){
+
+            if(this.data_rows <1 || this.data_cols == ""){
+
+              this.$notify({
+                title: 'Failed to Summon CSV',
+                message: 'Set right headers and row nums PLZ!',
+                type: 'warning',
+                offset:40,
+                position: 'top-left'
+              });
+              return;
+            }
+            
             var args = {}
             args['data_conf'] = this.tableData;
             args['date_start']  = this.date_start;
             args['data_rows'] = this.data_rows;
-            args['freq'] = this.freq;
+            args['freq'] =this.step +  this.freq;
             this.$http.post("/apply" , args).then(result => {
-              
-              console.log("====" , result);
+              this.data_cols_disabled = true;
+              // console.log("====" , result);
               this.retData.push(result.body);
               // console.log("====" , result);
+             
+              if(this.continue_time){
+                this.extend_time();
+              }
+              
               this.isdrawershow = true;
+              
+              this.$message({
+                title: 'Summon Success',
+                message: 'Summon Success',
+                type: 'success',
+                customClass:'messageIndex'
+              } );
+              
+             
+             
           });
         },
 
+        // delete batch data on temp ret drawer
+        del_item(index){
+            // console.log("======",e)
+            this.retData.splice(index , 1);
+            this.$notify({
+              title: "Done",
+              message: 'Delete Batch   [' + (index+1) + "]    Data Successful",
+              type: 'success',
+              offset:40,
+              position: 'top-left'
+            });
+        },
+        //export 2 csv
         downloads(){
+
+          if(this.retData.length < 1){
+            this.$notify({
+              title: 'Failed to Export CSV File',
+              message: 'Empty result,Summon it first',
+              type: 'warning',
+              offset:40,
+              position: 'top-left'
+            });
+            return;
+          }
           var header = this.data_cols.split(",")
-          // console.log("=====" , this.tableData)
-          // for(item in this.tableData){
-          //   header.push(item['name']);
-          // }
 
           var para = {};
           para["header"] = header;
@@ -82,7 +195,26 @@ new Vue({
               // this.onClose()
             }
           });
+        },
+        // see all btn click
+        showPreview(){
+          this.centerDialogVisible = true;
+          this.totalData = []
+          for(var i=0;i<this.retData.length;i++){
+            var batch = this.retData[i];
+            for(var j = 0 ; j<batch.length;j++){
+              var item = batch[j];
+              item['batch'] = i+1;
+              this.totalData.push(item);
+            }
+         }
+         this.totalData.sort((a, b) => new Date(a['index']).getTime()  - new Date(b['index']).getTime())
+        },
+
+        // set diff batch for diff bg-color
+        tableRowClassName(para){
+            // return "r"+row['batch']%10;
+            return "r"+ (para['row']['batch']%10);
         }
       }
-      
 })
